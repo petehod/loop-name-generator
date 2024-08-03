b1c3b6774b215970b56433f7a2fdb62526c4e387
petehod <pete@secretlab.com>
2024-08-03 09:00:03 -0500

    fix lint error

--- src/utils/openai.utils.ts
+++
@@ -1,7 +0,0 @@
-import OpenAI from "openai";
-
-export const openai = new OpenAI({
-  organization: process.env.NEXT_PUBLIC_ORGANIZATION_ID,
-  project: process.env.NEXT_PUBLIC_PROJECT_ID,
-  apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
-});
