d40e2f178c05a5f0f0b67de021cf0bb865da52a6
petehod <pete@secretlab.com>
2024-10-02 08:04:06 -0500

    redirect verified user away from verify email screen

--- src/app/verify-email/page.tsx
+++ src/app/verify-email/page.tsx
@@ -1,3 +1,24 @@
+"use client";
+import { useUser } from "@/context/UserContext";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function VerifyEmail() {
-  return <div>Please verify your email to use the application</div>;
+  const router = useRouter();
+  const { emailVerified } = useUser();
+
+  useEffect(() => {
+    if (emailVerified) router.push("/generate");
+  }, [emailVerified, router]);
+
+  return (
+    <div className="flex flex-col items-center justify-center gap-2 text-white  max-w-20 text-center">
+      <h2 className="text-white text-1.5 text-center ">
+        Please verify your email to use the application!
+      </h2>
+      <h3>
+        *If you successfully verified your email, try refreshing the page.
+      </h3>
+    </div>
+  );
 }
