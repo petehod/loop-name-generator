b66a3a97147b4a4719977dd2667a10b8b0e86a50
petehod <pete@secretlab.com>
2024-10-02 07:51:16 -0500

    make home redirect to generate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
 "use client";
 import { useRedirectLoggedInUser } from "@/hooks";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  const router = useRouter();
   useRedirectLoggedInUser();
-
-  return <div className="flex flex-col gap-4">Hi</div>;
+  useEffect(() => {
+    router.push("/generate");
+  }, [router]);
+  return null;
 }
