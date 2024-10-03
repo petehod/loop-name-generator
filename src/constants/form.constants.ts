e615e26ba1696e66586d73e755714f72669bd609
petehod <pete@secretlab.com>
2024-10-03 06:30:39 -0500

    add form input constants

---
+++ src/constants/form.constants.ts
@@ -0,0 +1,14 @@
+export const EMAIL_INPUT = {
+  minLength: 1,
+  maxLength: 40
+};
+
+export const PASSWORD_INPUT = {
+  minLength: 1,
+  maxLength: 20
+};
+
+export const USERNAME_INPUT = {
+  minLength: 1,
+  maxLength: 20
+};
