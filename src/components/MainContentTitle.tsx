bc10b122828ec5c734add56be16b874e7176d718
petehod <pete@secretlab.com>
2025-03-05 07:29:14 -0600

    update name

--- src/components/MainContentTitle.tsx
+++ src/components/MainContentTitle.tsx
@@ -3,8 +3,7 @@ import React from "react";
 export default function MainContentTitle() {
   return (
     <div className=" max-w-60 w-full  flex flex-col justify-center items-center pt-2">
-      <h2 className="text-2.5 text-center font-semibold">Quickie</h2>
-      <h1 className="text-1 font-light mb-3">Name Generator</h1>
+      <h2 className="text-2 text-center mb-3 font-semibold">Name Generator</h2>
     </div>
   );
 }
